1905927adbe9aed0926aeed60c93733c6cc2eba8
Andrea Michienzi <andrea.michienzi@di.unipi.it>
2020-10-28 18:43:43 +0100

    Update ContextTrustUpdater.java

--- src/main/java/eu/h2020/helios_social/core/trustmanager/ContextTrustUpdater.java
+++ src/main/java/eu/h2020/helios_social/core/trustmanager/ContextTrustUpdater.java
@@ -127,6 +127,8 @@ public class ContextTrustUpdater extends Thread {
         }
 
         while(!terminate) {
+            try { Thread.sleep(TrustManager.deltaT); }
+            catch (InterruptedException e) { e.printStackTrace(); }
             contextLock.lock();
             if(!active) {
                 try { contextCondVar.await(); }
@@ -134,8 +136,6 @@ public class ContextTrustUpdater extends Thread {
             }
             updateTrust();
             contextLock.unlock();
-            try { Thread.sleep(TrustManager.deltaT); }
-            catch (InterruptedException e) { e.printStackTrace(); }
         }
     }
 
